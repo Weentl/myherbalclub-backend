b4a187ab49b6f836c2a3e9c52b24e7e394932170
Tu Nombre <tuemail@example.com>
2024-08-20 11:38:27 -0600

    Descripción de los cambios

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
-});
+// Reemplaza <password> y <dbname> con tus credenciales
+const uri = 'mongodb+srv://wentl:Jangel05$@cluster0.eulny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
