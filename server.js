616bef0132db93427573a4f0d2dbb395d7817ec4
Tu Nombre <tuemail@example.com>
2024-08-20 20:20:37 -0600

    Actualiza configuración para MongoDB

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://wentl:<password>@cluster0.eulny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://wentl:Jangel05$@cluster0.eulny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
